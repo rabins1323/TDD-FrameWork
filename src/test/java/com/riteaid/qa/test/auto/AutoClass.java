ee430a6d3ed94706abcc825f9ea8240e8aa1060c
rabin1323 <rabinsytech@gmail.com>
2022-07-30 01:38:59 -0400

    com.riteaid.qa

---
+++ src/test/java/com/riteaid/qa/test/auto/AutoClass.java
@@ -0,0 +1,11 @@
+package com.riteaid.qa.test.auto;
+import org.testng.annotations.Test;
+import com.riteaid.qa.base.BaseClass;
+
+public class AutoClass extends BaseClass {
+
+public void findYourStore() {
+		
+	}
+}
+
